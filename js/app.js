39f9a8ac6ac25b8ae5a0a37a72865c556b49ded8
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-01 21:05:23 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,49 +1,44 @@
 // Make an array of objects (an array that will have objects) to store the student information and access it as hard-coded.
 
+// Information:
+
 let studentsInfo = [
   {
     name: "Shoaib",
     age: 19,
     regNo: 4863,
     course: "WebDevelopement",
-    favLanguages:
-     [
-      'html',
-      'css',
-      'java',
-      'c++'
-     ],
+    favLanguages: ["html", "css", "java", "c++"],
     marksOfSubjects: {
-      english: 60 ,
-      urdu: 55 ,
-      math: 75 ,
-      bio: 70 ,
+      english: 60,
+      urdu: 55,
+      math: 75,
+      bio: 70,
       physics: 60,
-    }
-
+    },
   },
   {
     name: "Taimoor",
     age: 20,
     regNo: 9120,
     course: "zoology",
-    favLanguages:
-     [
-      'turkish',
-      'spanish',
-      'arabic',
-      'pashto'
-     ],
+    favLanguages: ["turkish", "spanish", "arabic", "pashto"],
     marksOfSubjects: {
-      english: 72 ,
+      english: 72,
       pakStudies: 30,
-      math: 40 ,
-      bio: 68 ,
+      math: 40,
+      bio: 68,
       physics: 55,
-    }
+    },
   },
 ];
+let index = studentsInfo[studentsInfo.length - 1].favLanguages.length -1;
 console.log(studentsInfo);
-console.log(students[students.length - 1]); 
-console.log(students[students.length - 1].age); 
-console.log(students[students.length - 1].name);
+console.log(studentsInfo[studentsInfo.length - 1].name);
+console.log(studentsInfo[studentsInfo.length - 1].age);
+console.log(studentsInfo[studentsInfo.length - 1].regNo);
+console.log(studentsInfo[studentsInfo.length - 1].course);
+console.log(studentsInfo[studentsInfo.length - 1].favLanguages[0]);
+console.log(studentsInfo[studentsInfo.length - 1].favLanguages[index]);
+console.log("Marks Of English = " + studentsInfo[studentsInfo.length - 1].marksOfSubjects.english);
+console.log("Marks Of bio = " + studentsInfo[studentsInfo.length - 1].marksOfSubjects.bio);
